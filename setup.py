c356a778c2ffc73b466203b7a48a108a0f786fc2
Min RK <benjaminrk@gmail.com>
2015-12-13 16:31:01 +0100

    require pyreadline on Windows

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ install_requires = setuptools_args['install_requires'] = [
 extras_require = setuptools_args['extras_require'] = {
     'test:python_version=="2.7"': ['mock'],
     'test:sys_platform != "win32"': ['pexpect'],
+    ':sys_platform == "win32"': ['pyreadline'],
 
 }
 
