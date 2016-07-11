e979b4ff583d3e41176cee8849d246e060263136
Jacob Niehus <jacob.niehus@gmail.com>
2016-07-11 11:09:13 -0700

    Add command to test true color to true_color help

--- jupyter_console/ptshell.py
+++ jupyter_console/ptshell.py
@@ -136,7 +136,10 @@ class ZMQTerminalInteractiveShell(SingletonConfigurable):
     )
 
     true_color = Bool(False, config=True,
-        help="Use 24bit colors instead of 256 colors in prompt highlighting"
+        help=("Use 24bit colors instead of 256 colors in prompt highlighting. "
+              "If your terminal supports true color, the following command "
+              "should print 'TRUECOLOR' in orange: "
+              "printf \"\\x1b[38;2;255;100;0mTRUECOLOR\\x1b[0m\\n\"")
     )
 
     history_load_length = Integer(1000, config=True,
