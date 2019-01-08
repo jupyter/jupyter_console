0b9946dec92048d9ac8ab1f2caa1735957f194b8
Guy Sviry <32539816+guysv@users.noreply.github.com>
2019-01-08 20:27:46 +0200

    Remove ipykernel and jupyter_client Pinning

--- setup.py
+++ setup.py
@@ -76,9 +76,9 @@ if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
 
 setuptools_args = {}
 install_requires = setuptools_args['install_requires'] = [
-    'jupyter_client<6.0.0',
+    'jupyter_client',
     'ipython<6.0.0',
-    'ipykernel<5.0.0', # bless IPython kernel for now
+    'ipykernel', # bless IPython kernel for now
     'prompt_toolkit>=1.0.0,<2.0.0',
     'pygments',
 ]
